1977c1a40a524ea6dc4b5cc2bc114b8c6e1b40a1
Luan Ferreira <luanramosferr@outlook.com>
2022-11-05 11:42:16 -0300

    criando rotas

---
+++ server/src/routes/user.ts
@@ -0,0 +1,11 @@
+import { prisma } from "../lib/prisma"
+import { FastifyInstance } from "fastify"
+
+export async function userRoutes(fastify: FastifyInstance) {
+    fastify.get('/users/count', async () => {
+        const count = await prisma.user.count()
+        
+        return { count }
+    })
+
+}
